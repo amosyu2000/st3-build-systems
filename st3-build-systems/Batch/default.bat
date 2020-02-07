7a10565e9777395a778ac3e8dd90ed855fe0abaf
Amos Yu <amosyu2000@gmail.com>
2020-02-07 17:07:49 -0500

    added batch support

---
+++ st3-build-systems/Batch/default.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Python Build Batch File
+rem
+rem Repository: amosyu2000/st3-build-systems 
+rem Last Updated: 2019-12-12
+rem Author: Amos Yu - https://amosyu2000.github.io
+
+rem %~nx1 is the file name with extension
+
+start cmd /k "%~nx1"
